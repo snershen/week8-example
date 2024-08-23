1c279e9064de6a53095da53b72f732c47864a643
Wu Jane <jane72418@gmail.com>
2024-08-23 12:34:39 +0800

    init

---
+++ main.js
@@ -0,0 +1,36 @@
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+// const desktopNavDropdownMenu = document.querySelector(".desktop-nav-dropdown-menu");
+// const dropdownMenuLevelOne = document.querySelector(".dropdown-menu-level-one");
+// const dropdownMenuLevelTwo = document.querySelector(".dropdown-menu-level-two");
+// const dropdownMenuLevelThree = document.querySelector(".dropdown-menu-level-three");
+
+// function removeAllActiveItems(element) {
+//   const dropdownItems = element.querySelectorAll(".dropdown-item");
+//   dropdownItems.forEach((item) => {
+//     item.classList.remove("active");
+//   });
+// }
+
+// desktopNavDropdownMenu.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   if (e.target.matches(".dropdown-menu-level-one .dropdown-item")) {
+//     removeAllActiveItems(dropdownMenuLevelOne);
+//     e.target.classList.add("active");
+//     dropdownMenuLevelTwo.classList.add("d-block");
+//   }
+
+//   else if (e.target.matches(".dropdown-menu-level-two .dropdown-item")) {
+//     removeAllActiveItems(dropdownMenuLevelTwo);
+//     e.target.classList.add("active");
+//     dropdownMenuLevelThree.classList.add("d-block");
+//   }
+// });
+
+// const navDropdown = document.querySelector(".js-dropdown");
+// navDropdown.addEventListener("show.bs.dropdown", (event) => {
+//   removeAllActiveItems(desktopNavDropdownMenu);
+//   dropdownMenuLevelTwo.classList.remove('d-block');
+//   dropdownMenuLevelThree.classList.remove('d-block');
+// });
